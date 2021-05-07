f4e040d5d847d83cde9468eca60680d4ca9d298f
MzeDevelop <maik.hausner@googlemail.com>
2021-05-07 13:59:34 +0200

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,37 @@
+export const products = [
+  {
+    id: 1,
+    name: 'IPhone X Max',
+    price: 799,
+    description: 'Großes Handy mit OLED',
+    produced: 'Apple Inc'
+  },
+  {
+    id: 2,
+    name: 'IPhone 12 Mini',
+    price: 699,
+    description: 'Neuestes Handy mit guter Kamera',
+    produced: 'Apple Inc'
+  },
+  {
+    id: 3,
+    name: 'IPhone 12',
+    price: 299,
+    description: '',
+    produced: 'Apple Inc'
+  },
+  {
+    id: 4,
+    name: 'Samsung Galaxy s9',
+    price: 1050,
+    description: 'Faltbares Handy mit Ultra-Zoom Kamera',
+    produced: 'Samsung Inc'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
